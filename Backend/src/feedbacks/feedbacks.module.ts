23d53c9be6c962fe3b0795f37e0121b4d82aa0c9
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-12-16 02:48:49 +0600

    Feedback section Partially done

--- Backend/src/feedbacks/feedbacks.module.ts
+++ Backend/src/feedbacks/feedbacks.module.ts
@@ -1,7 +1,34 @@
+// import { Module } from '@nestjs/common';
+// import { FeedbacksService } from './feedbacks.service';
+
+// @Module({
+//   providers: [FeedbacksService],
+// })
+// export class FeedbacksModule {}
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FeedbacksService } from './feedbacks.service';
+import { FeedbacksController } from './feedbacks.controller';
+import { Sport } from 'src/sports/Sports_Entity/sports.entity';
+import { Student_Regi } from 'src/User/Student_Entity/student.entity';
+import { Feedback } from './Feedback_Entity/feedback.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Feedback, Sport, Student_Regi])],
   providers: [FeedbacksService],
+  controllers: [FeedbacksController],
 })
 export class FeedbacksModule {}
+
+// http://localhost:3000/feedbacks --> for run the file
+
+// Data --->
+// {
+//   "comment": "Great sport! Highly recommended.",
+//   "rating": 8,
+//   "sport": 1,
+//   "student": 2
+// }
+
+// http://localhost:3000/feedbacks/sport-popularity ---> for run the postman
